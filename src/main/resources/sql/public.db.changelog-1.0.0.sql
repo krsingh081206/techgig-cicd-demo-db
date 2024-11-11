d5aa19304a76f573152b4c79375edccf3660bbd8
krsingh081206 <kumar.ritesh@hsc.com>
2024-11-11 14:48:58 +0000

    Added orders table

--- src/main/resources/sql/public.db.changelog-1.0.0.sql
+++ src/main/resources/sql/public.db.changelog-1.0.0.sql
@@ -12,5 +12,14 @@ CREATE TABLE public.product (
 	CONSTRAINT product_pkey PRIMARY KEY (id)
 );
 
+CREATE TABLE public.orders (
+	id bigserial NOT NULL,
+	category varchar(50) DEFAULT 'NULL'::character varying NULL,
+	payment varchar(50) DEFAULT 'NULL'::character varying NULL,
+	description varchar(100) NOT NULL,
+	amount int4 NOT NULL,
+	CONSTRAINT order_pkey PRIMARY KEY (id)
+);
 
 --rollback drop table public.product;
+--rollback drop table public.orders;
